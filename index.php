d9a3a9a94025fa70171604ca42657a50fdef996c
clemence-briteau <clemence.briteau@imie.fr>
2018-03-28 19:48:26 +0200

    modification de la boucle pour avoir un tableau par serveur

--- index.php
+++ index.php
@@ -2,67 +2,86 @@
 
 include 'lib/functions.php';
 
-	$pdo = connectDb();
-	
-	$sql = $pdo ->query("SELECT * FROM `server`");
-    $stmnt = $pdo ->query("SELECT * FROM `service`");
+$pdo = connectDb();
+
+$sql = $pdo ->query("SELECT * FROM `server`");
+
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="robots" content="noindex">
-        <meta charset="utf-8">
-        <title>Server(s) Status</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
-    <body>
-    	<div class="container">
-    		<h3>Servers Status</h3>
-    		<table class="table table-bordered">
-				<tr>
-                    <th class="text-center">Name</th>
-					<th class="text-center">IP</th>
-					<th class="text-center">Status</th>
-				</tr>
-				<?php
-					while ($donnee = $sql->fetch()) {
-				
-				?>
-				 <tbody>
-                     <tr>
-                         <th style="text-align: center;"><?php echo $donnee['name']?></th>
-                         <td style="text-align: center;"><?php echo $donnee['host']?></td>
-                         <td style="text-align: center;"><?php pingIp($donnee['host']); ?></td>
-                     </tr>
-				</tbody>
-				<?php
-					}
-				?>
+<head>
+    <meta name="robots" content="noindex">
+    <meta charset="utf-8">
+    <title>Server(s) Status</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Servers Status</h3>
 
-			</table>
+        <?php
+        while ($donnee = $sql->fetch()) {
 
-			<form class="form-inline" role="form" action="lib/addServer.php" method="post">
-				<div class="form-group">
-					<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-				</div>
-				<div class="form-group">
-					<input type="text" class="form-control" onkeyup="javascript:checkForm(this)" id="host" name="host" placeholder="Domain / IP">
-				</div>
+        ?>
 
-				<button type="submit" class="btn btn-default" id="add-button">Add</button>
-			</form>
-			<br>
-    	</div>
-    	<script src="js/jquery.js" type="text/javascript"></script>
-        <script src="js/app.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    </body>
-		<footer>
+        <table class="table table-bordered table-hover">
+            <thead class="thead-light"> <!-- Ligne serveurs  -->
+            <tr>
+                    <th class="text-center"><?php echo $donnee['name']?></th>
+                    <th class="text-center"><?php echo $donnee['host']?></th>
+                    <th class="text-center"><?php pingIp($donnee['host']); ?></th>
+                </tr>
+            </thead> <!-- Fin ligne serveurs  -->
 
-		</footer>
+            <tbody> <!-- Tableau pour les services  -->
+            <?php
+                $stmnt = $pdo ->query("SELECT * FROM `service` WHERE idserver =". $donnee['idserver']);
+                while ($data = $stmnt->fetch()) { ?>
+
+<!--                    <table class="table table-bordered table-hover">-->
+<!--                        <tr>-->
+<!--                            <th class="text-center">--><?php //echo $data['name'] ?><!--</th>-->
+<!--                            <td class="text-center">--><?php //echo $data['socket'] ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    </table>-->
+
+                    <tr>
+                        <th class="text-center"><?php echo $data['name'] ?></th>
+                        <td class="text-center"><?php echo $data['socket'] ?></td>
+                    </tr>
+
+
+                    <?php
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+
+        <form class="form-inline" role="form" action="lib/addServer.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" onkeyup="javascript:checkForm(this)" id="host" name="host" placeholder="Domain / IP">
+            </div>
+
+            <button type="submit" class="btn btn-default" id="add-button">Add</button>
+        </form>
+        <br>
+    </div>
+
+    <footer>
+
+    </footer>
+
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/app.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</body>
 </html>
 
 
