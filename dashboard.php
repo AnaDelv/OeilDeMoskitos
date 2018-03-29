d15e1c9c7f6a65efcf2b49f5d9ac5c28d863dcee
Eleonore <eleonore.dehombreux@imie.fr>
2018-03-29 09:23:43 +0200

    resolving merge conflict

--- dashboard.php
+++ dashboard.php
@@ -15,54 +15,47 @@ $sql = $pdo ->query("SELECT * FROM `server`");
     <meta name="robots" content="noindex">
     <meta charset="utf-8">
     <title>Server(s) Status</title>
-<<<<<<< HEAD
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 <div class="container">
     <h3>Servers Status</h3>
+
+    <?php
+    while ($donnee = $sql->fetch()) {
+
+    ?>
+
     <table class="table table-bordered">
+        <!-- Ligne serveurs  -->
+        <thead class="thead-light">
         <tr>
-            <th class="text-center">Nom</th>
-            <th class="text-center">IP</th>
-            <th class="text-center">Statut</th>
-
-
+            <th class="text-center"><?php echo $donnee['name']?></th>
+            <th class="text-center"><?php echo $donnee['host']?></th>
+            <th class="text-center"><?php pingIp($donnee['host']); ?></th>
         </tr>
+        </thead>
+        <!-- Fin ligne serveurs  -->
+
+        <!-- Tableau pour les services  -->
+        <tbody>
         <?php
-        while ($donnee = $sql->fetch()) {
+        $stmnt = $pdo ->query("SELECT * FROM `service` WHERE idserver =". $donnee['idserver']);
+        while ($data = $stmnt->fetch()) { ?>
 
-            ?>
-            <tbody>
-            <!-- Tableau pour les serveurs  -->
             <tr>
-                <th style="text-align: center;"><?php echo $donnee['name']?></th>
-                <td style="text-align: center;"><?php echo $donnee['host']?></td>
-                <td style="text-align: center;"><?php pingIp($donnee['host']); ?></td>
-
-
+                <th class="text-center"><?php echo $data['name'] ?></th>
+                <td class="text-center"><?php echo $data['socket'] ?></td>
+                <!--<td class="text-center">--><?php //pingSocket($donnee['host'],$data['socket']); ?><!--</td>-->
             </tr>
-            </tbody>
+
             <?php
-            $stmnt = $pdo ->query("SELECT * FROM `service` WHERE idserver =". $donnee['idserver']);
-            while ($data = $stmnt->fetch()) { ?>
-
-                <tr>
-                    <!-- Tableau pour les services  -->
-                    <th style="text-align: center;"><?php echo $data['name'] ?></th>
-                    <td style="text-align: center;"><?php echo $data['socket'] ?></td>
-                    <!--                            <td style="text-align: center;">--><?php //pingSocket($donnee['host'],$data['socket']); ?><!--</td>-->
-
-
-                </tr>
-                </tbody>
-
-                <?php
-            }
+        }
         }
         ?>
-
+        </tbody>
     </table>
 
     <form class="form-inline" role="form" action="lib/addServer.php" method="post">
@@ -77,77 +70,13 @@ $sql = $pdo ->query("SELECT * FROM `server`");
     </form>
     <br>
 </div>
+
+<footer></footer>
+
 <script src="js/jquery.js" type="text/javascript"></script>
 <script src="js/app.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
-</body>
-<footer>
-
-</footer>
-=======
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <div class="container">
-        <h3>Servers Status</h3>
-
-        <?php
-        while ($donnee = $sql->fetch()) {
-
-        ?>
-
-        <table class="table table-bordered">
-            <!-- Ligne serveurs  -->
-            <thead class="thead-light">
-                <tr>
-                    <th class="text-center"><?php echo $donnee['name']?></th>
-                    <th class="text-center"><?php echo $donnee['host']?></th>
-                    <th class="text-center"><?php pingIp($donnee['host']); ?></th>
-                </tr>
-            </thead>
-            <!-- Fin ligne serveurs  -->
-
-            <!-- Tableau pour les services  -->
-            <tbody>
-                <?php
-                $stmnt = $pdo ->query("SELECT * FROM `service` WHERE idserver =". $donnee['idserver']);
-                while ($data = $stmnt->fetch()) { ?>
-
-                    <tr>
-                        <th class="text-center"><?php echo $data['name'] ?></th>
-                        <td class="text-center"><?php echo $data['socket'] ?></td>
-                        <!--<td class="text-center">--><?php //pingSocket($donnee['host'],$data['socket']); ?><!--</td>-->
-                    </tr>
-
-                    <?php
-                }
-            }
-            ?>
-            </tbody>
-        </table>
-
-        <form class="form-inline" role="form" action="lib/addServer.php" method="post">
-            <div class="form-group">
-                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" onkeyup="javascript:checkForm(this)" id="host" name="host" placeholder="Domain / IP">
-            </div>
-
-            <button type="submit" class="btn btn-default" id="add-button">Add</button>
-        </form>
-        <br>
-    </div>
-
-    <footer></footer>
-
-    <script src="js/jquery.js" type="text/javascript"></script>
-    <script src="js/app.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
 </body>
 
->>>>>>> 226da0227eed4fdd2de9b14695cbdfb1f6959389
 </html>
